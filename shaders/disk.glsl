1735aec69417769c0cd2ca4e3afd83c926b60f0b
minato-w <mina3710to2718@akane.waseda.jp>
2026-01-10 12:10:22 +0900

    Update disk.glsl

--- shaders/disk.glsl
+++ shaders/disk.glsl
@@ -1,7 +1,7 @@
-vec3 computeDisk(vec3 p) {
-    float distXZ = length(p.xz);
-    if (distXZ > 0.8 && distXZ < 2.5 && abs(p.y) < 0.02) {
-        float intensity = smoothstep(2.5, 0.8, distXZ);
+vec3 getAccretionDisk(vec3 p) {  // 引数が vec3 であること！
+    float dXZ = length(p.xz);
+    if(dXZ > 0.8 && dXZ < 2.5 && abs(p.y) < 0.02) {
+        float intensity = smoothstep(2.5, 0.8, dXZ);
         return vec3(1.0, 0.5, 0.2) * intensity;
     }
     return vec3(0.0);
