ae142c9911709d92838204d2858d914fa5db6e71
minato-w <mina3710to2718@akane.waseda.jp>
2026-01-10 11:43:06 +0900

    Update disk.glsl
    Calculations of Black Hole Accretion DiscsClick

--- shaders/disk.glsl
+++ shaders/disk.glsl
@@ -1,1 +1,8 @@
-
+vec3 computeDisk(vec3 p) {
+    float distXZ = length(p.xz);
+    if (distXZ > 0.8 && distXZ < 2.5 && abs(p.y) < 0.02) {
+        float intensity = smoothstep(2.5, 0.8, distXZ);
+        return vec3(1.0, 0.5, 0.2) * intensity;
+    }
+    return vec3(0.0);
+}
