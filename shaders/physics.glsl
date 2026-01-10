c9caa59d6f5199fa647d6355fb2a2d1091ed1d82
minato-w <mina3710to2718@akane.waseda.jp>
2026-01-10 12:20:34 +0900

    Update physics.glsl

--- shaders/physics.glsl
+++ shaders/physics.glsl
@@ -1,1 +1,5 @@
-
+void applyGravity(inout vec3 rd, vec3 p, float dt) {
+    float r2 = dot(p, p);
+    vec3 acceleration = -normalize(p) * (u_gravity / r2);
+    rd = normalize(rd + acceleration * dt);
+}
