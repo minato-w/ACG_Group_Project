c8af00346157053137f741a88040c4f28555988c
minato-w <mina3710to2718@akane.waseda.jp>
2026-01-10 12:43:20 +0900

    Update stars.glsl

--- shaders/stars.glsl
+++ shaders/stars.glsl
@@ -1,4 +1,10 @@
+float starHash(vec3 p) {
+    p = fract(p * 0.3183099 + 0.1);
+    p *= 17.0;
+    return fract(p.x * p.y * p.z * (p.x + p.y + p.z));
+}
+
 vec3 getBackground(vec3 rd) {
-    float stars = pow(fract(sin(dot(rd, vec3(12.9898, 78.233, 45.164))) * 43758.5453), 20.0);
+    float stars = pow(starHash(floor(rd * 200.0)), 20.0);
     return vec3(stars);
 }
